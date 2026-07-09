09429b0475ac84d9859de239daaa2cf5ef48eaf6
kirtisonje <kirtisonje2908@gmail.com>
2026-07-09 16:12:12 +0530

    remove files

--- app.py
+++ app.py
@@ -3,39 +3,11 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# MongoDB Atlas Connection
-client = MongoClient("mongodb+srv://kirtisonje2908_db_user:Kirti0909@cluster0.lorq5km.mongodb.net/?appName=Cluster0")
-
-db = client["student_db"]
-collection = db["students"]
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-@app.route("/submit", methods=["POST"])
-def submit():
-
-    try:
-        name = request.form["name"]
-        email = request.form["email"]
-
-        data = {
-            "name": name,
-            "email": email
-        }
-
-        collection.insert_one(data)
-
-        return render_template("success.html")
-
-    except Exception as e:
-        return render_template("index.html", error=str(e))
-
+@app.route('/api', methods=['GET'])
+def get_data():
+    with open ('data.json','r') as file:
+        data = json.load(file)
+        return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-    # Assignment branch update
+    app.run(debug=True) 
